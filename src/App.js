ceb34b933aca5aad1598d076f147432041a4ca8d
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-08 16:09:10 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/navbar/Navigation';
 import Banner from './components/banner/banner';
+import About from './components/about/about';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 }
