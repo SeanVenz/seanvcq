05220e09dc81ffaecad7451d8eac05443491c8b6
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-09 22:56:52 +0800

    rawr

--- src/components/about/interest.js
+++ src/components/about/interest.js
@@ -5,42 +5,43 @@ import eating from "../../images/cutlery.png";
 import motorRiding from "../../images/motorbike.png";
 import gaming from "../../images/gamer.png";
 import volleyball from "../../images/volleyball-ball.png";
+import './interest.scss'
 
 function Interests() {
   return (
-    <div className="about" id="about">
-      <div className="about-heading">
+    <div className="interests">
+      <div className="interests-heading">
         <h2>
           <b>Interests</b>
         </h2>
       </div>
-      <div className="about-description">
-        <div className="description">
-          <div className="info">
-            <img src={eating} alt="Eating" />
-            <p>Eating</p>
+      <div className="interests-container">
+        <div className="column">
+          <div className="indiv-interest">
+            <img src={eating} alt="Eating" className="img-hover" />
+            <p className="left">EATING</p>
           </div>
-          <div className="info">
-            <img src={poetry} alt="Poetry" />
-            <p>Poetry</p>
+          <div className="indiv-interest">
+            <img src={poetry} alt="Poetry" className="img-hover"/>
+            <p className="right">POETRY</p>
           </div>
-          <div className="info">
-            <img src={gaming} alt="Gaming" />
-            <p>Gaming</p>
+          <div className="indiv-interest">
+            <img src={gaming} alt="Gaming" className="img-hover"/>
+            <p className="left">GAMING</p>
           </div>
         </div>
-        <div className="my-column">
-          <div className="info">
-            <img src={badminton} alt="Badminton" />
-            <p>Badminton</p>
+        <div className="column">
+          <div className="indiv-interest">
+            <img src={badminton} alt="Badminton" className="img-hover"/>
+            <p className="right">BADMINTON</p>
           </div>
-          <div className="info">
-            <img src={motorRiding} alt="Motorcycle Riding" />
-            <p>Motorcycle Riding</p>
+          <div className="indiv-interest">
+            <img src={motorRiding} alt="Motorcycle Riding" className="img-hover"/>
+            <p className="left">MOTORCYCLE RIDING</p>
           </div>
-          <div className="info">
-            <img src={volleyball} alt="Volleyball" />
-            <p>Volleyball</p>
+          <div className="indiv-interest">
+            <img src={volleyball} alt="Volleyball" className="img-hover"/>
+            <p className="right">VOLLEYBALL</p>
           </div>
         </div>
       </div>
