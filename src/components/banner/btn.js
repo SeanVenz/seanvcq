be39e2a50254ec01fd5cccd8fd2cd56216f841e3
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-21 22:54:48 +0800

    asd

--- src/components/banner/btn.js
+++ src/components/banner/btn.js
@@ -1,19 +1,19 @@
 import React from "react";
-import './button.scss'
+import "./button.scss";
 
 function Btn() {
-  return (
-    <button class="btn" type="button">
-      <strong>CONTACT ME </strong>
-      <div id="container-stars">
-        <div id="stars"></div>
-      </div>
 
-      <div id="glow">
-        <div class="circle"></div>
-        <div class="circle"></div>
-      </div>
-    </button>
+  return (
+      <a href="#contact-me"><button class="btn" type="button">
+        <strong>CONTACT ME</strong>
+        <div id="container-stars">
+          <div id="stars"></div>
+        </div>
+        <div id="glow">
+          <div class="circle"></div>
+          <div class="circle"></div>
+        </div>
+      </button></a> 
   );
 }
 
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./contacs.scss";
-import ContactForm from "./ContactForm";
+import ContactForm from "./ContactForm"; 
 
 function Contact() {
   return (
-    <div className="contact-me">
+    <div className="contact-me" id="contact-me">
       <div className="contact-me-container">
+        <div className="contact-heading">
+          <h2>CONTACT ME</h2>
+        </div>
         <div className="contact-desc">
           <span>
             Thinking of a project? Reach out to me and we could do it today!
