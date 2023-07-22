3e12ab967f1ccab23ff7d4a60485de30f8bf8394
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-22 22:26:48 +0800

    contacct

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,50 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./contactform.scss";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
+
+const TIME_TO_SHOW_AGAIN = 30 * 60 * 1000; // 30 minutes in milliseconds
 
 function ContactForm() {
+  const [messageSent, setMessageSent] = useState(false);
+
+  useEffect(() => {
+    const sendTime = localStorage.getItem("sendTime");
+    const currentTime = new Date().getTime();
+
+    if (sendTime && currentTime - sendTime < TIME_TO_SHOW_AGAIN) {
+      setMessageSent(true);
+
+      setTimeout(() => {
+        setMessageSent(false);
+        localStorage.removeItem("sendTime");
+      }, sendTime + TIME_TO_SHOW_AGAIN - currentTime);
+    }
+  }, []);
+
   const sendEmail = (event) => {
     event.preventDefault();
 
-    emailjs.sendForm('service_7kzxz87', 'template_jih3xin', event.target, 'Hwy3DTSecMURqGo0X')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_7kzxz87",
+        "template_jih3xin",
+        event.target,
+        "Hwy3DTSecMURqGo0X"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          setMessageSent(true);
+          localStorage.setItem("sendTime", new Date().getTime());
+
+          setTimeout(() => {
+            setMessageSent(false);
+            localStorage.removeItem("sendTime");
+          }, TIME_TO_SHOW_AGAIN);
+        },
+        (error) => {
           console.log(error.text);
-      });
-  }
+        }
+      );
+  };
 
   return (
     <div className="form-container">
-      <form className="form" onSubmit={sendEmail}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" required="" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" required="" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" name="subject" required="" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="textarea">How Can I Help You?</label>
-          <textarea
-            name="textarea"
-            id="textarea"
-            rows="10"
-            cols="50"
-            required=""
-          >
-            {" "}
-          </textarea>
-        </div>
-        <button className="form-submit-btn" type="submit">
-          Submit
-        </button>
-      </form>
+      {messageSent ? (
+        <div className="message-sent">Your message has been sent successfully!</div>
+      ) : (
+        <form className="form" onSubmit={sendEmail}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" required="" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" required="" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="subject">Subject</label>
+            <input type="text" id="subject" name="subject" required="" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="textarea">How Can I Help You?</label>
+            <textarea
+              name="textarea"
+              id="textarea"
+              rows="10"
+              cols="50"
+              required=""
+            >
+              {" "}
+            </textarea>
+          </div>
+          <button className="form-submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 }
