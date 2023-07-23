e7ddf48ca5a4df1795c69361989f225d4035610c
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-23 19:52:23 +0800

    asd

--- src/components/navbar/Navigation.js
+++ src/components/navbar/Navigation.js
@@ -1,14 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import "./Nav.scss";
 import logo from "../../images/logo.png";
 
 function Navigation() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const updateScroll = () => {
+      setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
+    window.addEventListener('scroll', updateScroll);
+    return () => window.removeEventListener('scroll', updateScroll);
+  }, []);
+
   return (
     <div className="navigation">
       <div className="link">
         <a href="#home">HOME</a>
         <a href="#about">ABOUT ME</a>
         <a href="#home">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" style={{height: scrollPosition > 200 ? '50px' : '100px', width: scrollPosition > 200 ? '50px' : '100px'}} />
         </a>
         <a href="#skills">SKILLS</a>
         <a href="#projects">PROJECTS</a>
