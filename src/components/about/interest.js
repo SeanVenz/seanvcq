3ff8ac112568fa3c54efee60db91ac6fbb5b7047
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-07-25 21:36:11 +0800

    asd

--- src/components/about/interest.js
+++ src/components/about/interest.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.net.min";
+import * as THREE from "three";import React from "react";
 import poetry from "../../images/poetry.jpg";
 import badminton from "../../images/badminton.jpg";
 import eating from "../../images/eating.jpg";
@@ -8,8 +10,35 @@ import volleyball from "../../images/volleyball.jpg";
 import "./interest.scss";
 
 function Interests() {
+
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: vantaRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 600.0,
+          minWidth: 600.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          backgroundAlpha: 0.00,
+          points: 4.00
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="interests">
+    <div className="interests" ref={vantaRef}>
       <div className="interest-heading">
         <h2>
           <b>- interests -</b>
