50cf6898fc25024c8258ce072c8e09ffc5d6577e
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-11-23 21:35:10 +0800

    test

--- src/components/about/interest.js
+++ src/components/about/interest.js
@@ -1,12 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import BIRDS from "vanta/dist/vanta.net.min";
 import * as THREE from "three";import React from "react";
-import poetry from "../../images/poetry.jpg";
-import badminton from "../../images/badminton.jpg";
-import eating from "../../images/eating.jpg";
-import motorRiding from "../../images/motorcylce.jpg";
-import gaming from "../../images/gaming.jpg";
-import volleyball from "../../images/volleyball.jpg";
 import "./interest.scss";
 
 function Interests() {
@@ -23,8 +17,8 @@ function Interests() {
           mouseControls: true,
           touchControls: true,
           gyroControls: false,
-          minHeight: 600.0,
-          minWidth: 600.0,
+          minHeight: 350.0,
+          minWidth: 350.0,
           scale: 1.0,
           scaleMobile: 1.0,
           backgroundAlpha: 0.00,
@@ -47,27 +41,21 @@ function Interests() {
       <div className="my-interests">
         <div className="left indiv-interest">
           <p>EATING</p>
-          <img src={eating} alt="Eating" />
         </div>
         <div className="right indiv-interest">
           <p>GAMING</p>
-          <img src={gaming} alt="Gaming" />
         </div>
         <div className="left indiv-interest">
           <p>POETRY</p>
-          <img src={poetry} alt="Poetry" />
         </div>
         <div className="right indiv-interest">
           <p>BADMINTON</p>
-          <img src={badminton} alt="Badminton" />
         </div>
         <div className="left indiv-interest">
           <p>VOLLEYBALL</p>
-          <img src={volleyball} alt="Volleyball" />
         </div>
         <div className="right indiv-interest">
           <p>MOTORCYCLE RIDING</p>
-          <img src={motorRiding} alt="Motorcycle-Riding" />
         </div>
       </div>
     </div>
