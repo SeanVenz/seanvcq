2597e60d301a7c0a2e8888bdf5e3d1eaa2ab02b5
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-11-23 22:55:44 +0800

    adjustments

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,56 +1,96 @@
 import React from "react";
 import "./projects.scss";
 import interpreter from "../../images/interpreter.jpg";
-import identity from '../../images/identity.jpg'
-import coffeeshop from '../../images/cofee-shop.jpg'
-import barangayApp from '../../images/barangay-reg.png'
+import identity from "../../images/identity.jpg";
+import coffeeshop from "../../images/cofee-shop.jpg";
+import barangayApp from "../../images/barangay-reg.png";
 import ProjectCard from "./ProjectCard";
-import carRental from '../../images/car-rental.jpg'
-import careerAI from '../../images/careerAI.png'
-import meanTemp from '../../images/meantemp.jpg'
-import walletWise from '../../images/walletwise.png'
+import carRental from "../../images/car-rental.jpg";
+import careerAI from "../../images/careerAI.png";
+import meanTemp from "../../images/meantemp.jpg";
+import walletWise from "../../images/walletwise.png";
 
 function Projects() {
   const projects = [
     {
       name: "Wallet Wise",
       img: walletWise,
-      points: ["Made using ReactJS and Firebase", "Let Vendor add Foods", "Let students order food", "Let students order and deliver foods", "Let students converse with each other", "Let students view location of courier and buyer"]
+      points: [
+        "Made using ReactJS and Firebase",
+        "Let Vendor add Foods",
+        "Let students order food",
+        "Let students order and deliver foods",
+        "Let students converse with each other",
+        "Let students view location of courier and buyer",
+      ],
     },
     {
       name: "Interpreter",
       img: interpreter,
-      points: ["Made using ANTLR and C#", "Follows CODE Interpreter guidelines set by Professor", "Has complete functionalities like loop, switch, and conditional statements."]
+      points: [
+        "Made using ANTLR and C#",
+        "Follows CODE Interpreter guidelines set by Professor",
+        "Has complete functionalities like loop, switch, and conditional statements.",
+      ],
     },
     {
       name: "Family Registration App",
       img: barangayApp,
-      points: ["Made using C# and ReactJS", "Can Register, Update, and Delete Family, Barangays and Family Members.", "Complete Front-End, Back-End, SQL Database, and Unit Testing"]
+      points: [
+        "Made using C# and ReactJS",
+        "Can Register, Update, and Delete Family, Barangays and Family Members.",
+        "Complete Front-End, Back-End, SQL Database, and Unit Testing",
+      ],
     },
     {
       name: "CareerAI",
       img: careerAI,
-      points: ["Made using C# and ReactJS", "Uses ML .NET", "Was trained using 6k rows and 20 columns of dataset", "Can predict your Career based on your knowledge on IT Fields"]
+      points: [
+        "Made using C# and ReactJS",
+        "Uses ML .NET",
+        "Was trained using 6k rows and 20 columns of dataset",
+        "Can predict your Career based on your knowledge on IT Fields",
+      ],
     },
     {
       name: "Mean Temperature Prediction",
       img: meanTemp,
-      points: ["Made using C#", "Uses Backpropagation Algorithm", "Can predict annual temperature based on Carbon Dioxide Emission"]
+      points: [
+        "Made using C#",
+        "Uses Backpropagation Algorithm",
+        "Can predict annual temperature based on Carbon Dioxide Emission",
+      ],
     },
     {
       name: "Coffee Shop API",
       img: coffeeshop,
-      points: ["Made using C#", "Can Register, Update, and Delete Barista, Order, Customer, Customer Preferences.", "Complete Back-End and SQL Database"]
+      points: [
+        "Made using C#",
+        "Can Register, Update, and Delete Barista, Order, Customer, Customer Preferences.",
+        "Complete Back-End and SQL Database",
+      ],
     },
     {
       name: "Identity API",
       img: identity,
-      points: ["Made using C#", "Combines Other Free API's to generate identity", "Deployed using Azure", "Has CI/CD", "Applies SOLID Principles", "Applies Design Pattern", "Complete Back-End, Unit Testing, SQL Database"]
+      points: [
+        "Made using C#",
+        "Combines Other Free API's to generate identity",
+        "Deployed using Azure",
+        "Has CI/CD",
+        "Applies SOLID Principles",
+        "Applies Design Pattern",
+        "Complete Back-End, Unit Testing, SQL Database",
+      ],
     },
     {
       name: "Car Rental Management System",
       img: carRental,
-      points: ["Made using Java JDBC", "Can book a Car with driver", "Can see car Availability."]
+      points: [
+        "Made using Java JDBC",
+        "Can book a Car with driver",
+        "Can see car Availability.",
+      ],
     },
   ];
 
@@ -71,7 +111,7 @@ function Projects() {
             img={project.img}
             title={project.name}
             description={project.description}
-            points = {project.points}
+            points={project.points}
           />
         ))}
       </div>
