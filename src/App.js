c21e27411a9b2816c90886eb151e4a70420b0da8
shan <quijano.seanvenz@gmail.com>
2024-11-17 18:49:53 +0800

    v2 init

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Navigation from './components/navbar/Navigation';
-import Banner from './components/banner/banner';
-import About from './components/about/about';
-import Skills from './components/skills/skills';
-import Interests from './components/about/interest';
-import Projects from './components/projects/Projects';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-import Certifications from './components/skills/Certifications';
-import React, { useState, useEffect } from 'react';
-import Loader from './components/Loader';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  
   return (
     <div className="App">
-      <Navigation></Navigation>
-      <Banner></Banner>
-      <About></About>
-      <Interests></Interests>
-      <Skills  />
-      {/* <Certifications/> */}
-      <Projects  />
-      <Contact />
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
