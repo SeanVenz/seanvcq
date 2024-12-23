7691e18b8e4869cd6a03082ce3089bf9017b72da
shan <quijano.seanvenz@gmail.com>
2024-12-23 11:39:06 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import UnderConstruction from './components/UnderConstruction';
 
 function App() {
   return (
-    <main className='bg-main w-full h-screen flex items-center justify-center'>
-      {/* <Navbar/>
+    <main className='bg-main w-full h-full'>
+      <Navbar/>
       <Home/>
-      <About/> */}
-      <UnderConstruction/>
+      <About/>
     </main>
   );
 }
