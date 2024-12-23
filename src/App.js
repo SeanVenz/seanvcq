deb9b2db760fd37974126c8e12fe940ba870ad96
shan <quijano.seanvenz@gmail.com>
2024-12-23 11:36:01 +0800

    under construction

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import UnderConstruction from './components/UnderConstruction';
 
 function App() {
   return (
-    <main className='bg-main w-full h-full'>
-      <Navbar/>
+    <main className='bg-main w-full h-screen flex items-center justify-center'>
+      {/* <Navbar/>
       <Home/>
-      <About/>
+      <About/> */}
+      <UnderConstruction/>
     </main>
   );
 }
